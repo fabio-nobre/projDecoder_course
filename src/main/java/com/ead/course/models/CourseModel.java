cbbb816da4a68a5fecfdef302197473eb69771da
fabio-nobre <f.oliveiranobre@gmail.com>
2024-06-05 14:47:47 -0300

    iniciando comunicacao assincrona

--- src/main/java/com/ead/course/models/CourseModel.java
+++ src/main/java/com/ead/course/models/CourseModel.java
@@ -51,13 +51,4 @@ public class CourseModel implements Serializable {
     @OneToMany(mappedBy = "course", fetch = FetchType.LAZY )
     @Fetch(FetchMode.SUBSELECT)
     private Set<ModuleModel> modules;
-
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
-    private Set<CourseUserModel> coursesUsers;
-
-    public CourseUserModel convertToCourseUserModel(UUID userID){
-        return new CourseUserModel(null, userID, this);
-    }
-
 }
